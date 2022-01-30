04fd75649f2f1ca10a16e934f5037f14b9005b03
Noushedul1 <noushedakib@gmail.com>
2022-01-30 11:45:53 +0600

    add a callback function

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const post = [
+const posts = [
     {title: 'front end developer',body: 'you are a developer'},
     {title: 'back end developer',body: 'you are a back end developer'}
 ]
 const getPost = ()=>{
     setTimeout(()=>{
         let output = '';
-        post.forEach((p)=>{
+        posts.forEach((p)=>{
             output += `<li>${p.title}</li>`;
             document.body.innerHTML = output;
         });
     },2000);
 }
-getPost()
+const createPost = (post,calback)=>{
+    setTimeout(()=>{
+        posts.push(post);
+    },2000);
+    calback();
+}
+createPost({title: 'ui/ux designer', body: 'joy is a ui/ux designer'},getPost);
